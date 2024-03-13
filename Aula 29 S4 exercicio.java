c44e57d0de288501227760c9d7c2a53cff61b216
JullianeMarie <133807095+JullianeMarie@users.noreply.github.com>
2024-03-13 20:52:26 -0300

    Este projeto contém um programa em Java que calcula a área de três círculos com base nos raios fornecidos pelo usuário

---
+++ Aula 29 S4 exercicio.java
@@ -0,0 +1,34 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		double raio1, raio2, raio3, area1, area2, area3;
+		raio1 = sc.nextDouble();
+		raio2 = sc.nextDouble();
+		raio3 = sc.nextDouble();
+		area1 = 3.14159 * Math.pow(raio1, 2.0);
+		area2 = 3.14159 * Math.pow(raio2, 2.0);
+		area3 = 3.14159 * Math.pow(raio3, 2.0);
+
+		System.out.printf(" Area 1 é igual: %.4f%n", area1);
+		System.out.printf(" Area 2 é igual: %.4f%n", area2);
+		System.out.printf(" Area 3 é igual: %.4f%n", area3);
+
+		sc.close();
+
+	}
+}
+
+/*
+ * 2.00 100.64 150.00 Area 1 é igual: 12.5664 Area 2 é igual: 31819.3103 Area 3
+ * é igual: 70685.7750
+ */
