5f8fbf817b07dca16ba1fbd783f1621a0c157312
JullianeMarie <133807095+JullianeMarie@users.noreply.github.com>
2024-03-14 15:52:24 -0300

    Condicional Ternaria

---
+++ Aula 39 CondicionalTernaria.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		double preço = sc.nextDouble();
+		double desconto = (preço > 20.00)? preço * 0.1 : preço * 0.05;
+		
+				
+		System.out.printf(" O valor do desconto R$ %.2f%n ", desconto );
+
+
+		sc.close();
+
+	}
+}
+
+
+
+
