43f8626805ac22b2d15222b887787cce0235341c
JullianeMarie <133807095+JullianeMarie@users.noreply.github.com>
2024-03-13 21:25:59 -0300

    Exercício de Cálculo da Diferença

---
+++ Aula 29 S4 exercicio3.java
@@ -0,0 +1,32 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		int A, B, C, D, diferenca;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		diferenca = (A * B - C * D);
+		System.out.println("DIFERENÇA = " + diferenca);
+
+		sc.close();
+
+	}
+}
+/*
+ * 5 
+ * 6 
+ * -7 
+ * 8 
+ * DIFERENÇA = 86
+ */
