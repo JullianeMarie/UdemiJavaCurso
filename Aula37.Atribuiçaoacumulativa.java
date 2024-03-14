4680709863bb06f8991f8ea9cd7b13c41388a380
JullianeMarie <133807095+JullianeMarie@users.noreply.github.com>
2024-03-14 15:36:38 -0300

    Atribuição Acumulativa

---
+++ Aula37.Atribuiçaoacumulativa.java
@@ -0,0 +1,32 @@
+﻿
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+		int minutos = sc.nextInt();
+		double conta = 50.00;
+
+		if (minutos > 100) {
+			conta += (minutos - 100) * 2.00;
+		}
+		System.out.printf(" Valor a pagar: R$ %.2f%n ", conta);
+
+		sc.close();
+
+	}
+}
+/*
+ * 113 
+ * Valor a pagar: R$ 76.00
+ * Observação: No exemplo usado: conta = conta + ( minutos - 100 ) * 2.0; A
+ * atribuição é colocada conta += ( minutos - 100 ) * 2.0;
+ */
