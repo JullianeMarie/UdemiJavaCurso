d3a0ef86551046cb0a266d65cd1156abb9de8fbf
JullianeMarie <133807095+JullianeMarie@users.noreply.github.com>
2024-03-16 06:09:01 -0300

    Exercício de Verificação de Número Par ou Ímpar em Java

---
+++ Aula 36.ex2.java
@@ -0,0 +1,28 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+
+		Scanner sc = new Scanner(System.in);
+
+	int numero = sc.nextInt();
+		 
+	if (numero % 2 == 0) {
+		System.out.println(" PAR ");
+	}
+	else if (numero % 2 != 0) {
+		System.out.println(" IMPAR ");
+	}
+				
+sc.close();
+		}	
+	}
+
+27
+ IMPAR 
